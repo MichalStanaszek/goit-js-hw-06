5a7e19472e01b8ac0ebc765842f2408e4007281b
MichalStanaszek <michal.stanaszek@gmail.com>
2023-05-28 00:25:05 +0200

    Update task-02.js
    task 1/4 all
    task 2/4 part

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// 1. Utworzy oddzielny element <li>. Obowiązkowo użyj metody document.createElement().
+const list = document.createElement("li");
+console.log(list);
+
+// 2. Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  console.log(ingredients[i]);
+}
