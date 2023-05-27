9942b33fbea33e53119521db4bedad3d26946188
MichalStanaszek <michal.stanaszek@gmail.com>
2023-05-27 23:19:05 +0200

    Update task-01.js
    task 1/2

---
+++ js/task-01.js
@@ -0,0 +1,3 @@
+// 1. Policzy i wypisze w konsoli liczbę kategorii w ul#categories, czyli elementy li.item.
+const items = document.querySelectorAll(".item");
+console.log(`Number of categories: ${items.length}`);
