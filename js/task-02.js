1167047e8b84292fff1f98f03dfafa805668632b
MichalStanaszek <michal.stanaszek@gmail.com>
2023-05-28 20:31:12 +0200

    Update task-02.js
    użycie pętli for do zoptymalizowania kodu

--- js/task-02.js
+++ js/task-02.js
@@ -7,45 +7,18 @@ const ingredients = [
   "Condiments",
 ];
 
-// 1. Utworzy oddzielny element <li>. Obowiązkowo użyj metody document.createElement().
-
 const listOfIngredients = document.querySelector("#ingredients");
-const firstItem = document.createElement("li");
-const secondItem = document.createElement("li");
-const thirdItem = document.createElement("li");
-const forthItem = document.createElement("li");
-const fifthItem = document.createElement("li");
-const sixthItem = document.createElement("li");
 
-// 2. Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
+for (let i = 0; i < ingredients.length; i += 1) {
+  let list = document.createElement("li");
+  list.textContent = ingredients[i];
+  list.classList.add("item");
+  listOfIngredients.appendChild(list);
+}
 
-firstItem.textContent = ingredients[0];
-secondItem.textContent = ingredients[1];
-thirdItem.textContent = ingredients[2];
-forthItem.textContent = ingredients[3];
-fifthItem.textContent = ingredients[4];
-sixthItem.textContent = ingredients[5];
-
-// 3. Doda do elementu klasę item.
-firstItem.classList.add("item");
-secondItem.classList.add("item");
-thirdItem.classList.add("item");
-forthItem.classList.add("item");
-fifthItem.classList.add("item");
-sixthItem.classList.add("item");
-
-// 4. Następnie jedna operacja umieści wszystkie <li> na liście ul#ingredients.
-
-listOfIngredients.append(firstItem);
-listOfIngredients.append(secondItem);
-listOfIngredients.append(thirdItem);
-listOfIngredients.append(forthItem);
-listOfIngredients.append(fifthItem);
-listOfIngredients.append(sixthItem);
-
-// To też zadziała
-// const markup = ingredients
-//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
-//   .join("");
-// console.log(markup);
-// listOfIngredients.innerHTML = markup;
+// // To też zadziała
+// // const markup = ingredients
+// //   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+// //   .join("");
+// // console.log(markup);
+// // listOfIngredients.innerHTML = markup;
