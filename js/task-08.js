71da87944e6eb837ebd0daa56db12a52781a991b
MichalStanaszek <michal.stanaszek@gmail.com>
2023-05-31 21:02:07 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,10 @@
 const form = document.querySelector(".login-form");
 
-// 1. Funkcjonalność przesyłania formularza form.login-form powinno zostać obsłużona
-// przy użycie wydarzenia submit
 form.addEventListener("submit", handleSubmit);
 
-// 2. Podczas przesyłania formularza strona nie powinna się odświeżać.
 function handleSubmit(event) {
   event.preventDefault();
 
-  //   3. Jeśli w formularzu są nieuzupełnione pola,
-  //   wyświetl alert z upomnieniem o tym, że wszystkie pola powinny zostać wypełnione.
   const {
     elements: { email, password },
   } = event.currentTarget;
@@ -17,9 +12,7 @@ function handleSubmit(event) {
   if (email.value === "" || password.value === "") {
     alert("Please fill in all the fields!");
   }
-}
 
-// 4. Jeśli użytkownik uzupełnił wszystkie pola i wysłał formularz,
-// pobierz wartość pól jako obiekt, gdzie nazwa pola będzie nazwą właściwości,
-// a wartość pola - wartością właściwości. A
-// by otrzymać dostęp do elementów formularza użyj właściwości elements.
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
