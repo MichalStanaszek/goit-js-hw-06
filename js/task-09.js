b9683b80795e563b47cf88959afe856084540abd
MichalStanaszek <michal.stanaszek@gmail.com>
2023-05-31 23:04:11 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+const setBg = () => {
+  let result = getRandomHexColor();
+  document.body.style.backgroundColor = result;
+  span.innerHTML = result;
+};
+
+button.addEventListener("click", setBg);
