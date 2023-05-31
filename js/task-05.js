1165cac715d8839a3538432185e445b7f7036529
MichalStanaszek <michal.stanaszek@gmail.com>
2023-05-31 23:14:42 +0200

    Update task-05.js
    add blur event with "Anonymous" string

--- js/task-05.js
+++ js/task-05.js
@@ -4,3 +4,9 @@ const textOutput = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
   textOutput.textContent = event.currentTarget.value;
 });
+
+textInput.addEventListener("blur", (event) => {
+  if (event.currentTarget.value === "") {
+    textOutput.textContent = "Anonymous";
+  }
+});
