7799330ba80f00b379783ea1a1865799b633a686
MichalStanaszek <michal.stanaszek@gmail.com>
2023-06-01 17:36:14 +0200

    Update task-04.js
    - create variable "counter" and use it later on buttons

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,15 @@
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
+const counter = document.getElementById("value");
 
 let counterValue = 0;
 
 decrementBtn.addEventListener("click", () => {
   counterValue -= 1;
-  document.getElementById("value").innerHTML = counterValue;
+  counter.innerHTML = counterValue;
 });
 
 incrementBtn.addEventListener("click", () => {
   counterValue += 1;
-  document.getElementById("value").innerHTML = counterValue;
+  counter.innerHTML = counterValue;
 });
