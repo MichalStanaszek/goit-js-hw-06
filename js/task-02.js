ebda911c199cdb38d1488323d06d86292b17806e
MichalStanaszek <michal.stanaszek@gmail.com>
2023-06-02 22:48:39 +0200

    Update task-02.js
    refactor whole task

--- js/task-02.js
+++ js/task-02.js
@@ -7,16 +7,26 @@ const ingredients = [
   "Condiments",
 ];
 
-const listOfIngredients = document.querySelector("#ingredients");
-
-ingredients.forEach(function (ingredient, index) {
-  let list = document.createElement("li");
-  list.textContent = ingredients[index];
-  list.classList.add("item");
-  listOfIngredients.append(list);
+const list = document.querySelector("#ingredients");
+const itemsList = ingredients.map((ingredient) => {
+  const lastElement = document.createElement("li");
+  lastElement.textContent = ingredient;
+  lastElement.classList.add("item");
+  return lastElement;
 });
+list.append(...itemsList);
 
-// // To też zadziała
+// -----------------------------------
+// const listOfIngredients = document.querySelector("#ingredients");
+
+// ingredients.forEach(function (ingredient, index) {
+//   let list = document.createElement("li");
+//   list.textContent = ingredients[index];
+//   list.classList.add("item");
+//   listOfIngredients.append(list);
+// });
+
+// -----------------------------------
 // for (let i = 0; i < ingredients.length; i += 1) {
 //   let list = document.createElement("li");
 //   list.textContent = ingredients[i];
@@ -25,8 +35,8 @@ ingredients.forEach(function (ingredient, index) {
 // }
 
 // -----------------------------------
-// // const markup = ingredients
-// //   .map((ingredient) => `<li class="item">${ingredient}</li>`)
-// //   .join("");
-// // console.log(markup);
-// // listOfIngredients.innerHTML = markup;
+// const markup = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join("");
+// console.log(markup);
+// listOfIngredients.innerHTML = markup;
